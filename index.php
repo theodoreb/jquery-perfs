0afebe7eb215b9b4cdb904284ea4ea0c64c440fc
Théodore Biadala <theodore.biadala@acquia.com>
2012-05-05 20:01:05 +0200

    Mieux mieux

--- index.php
+++ index.php
@@ -20,28 +20,46 @@ function load($c) {
 
 $component = !empty($_GET['c']) ? $_GET['c'] : 'raw';
 $query = qs($component);
-$samples = !empty($_GET['s']) ? $_GET['s'] : 10;
+$samples = !empty($_GET['s']) ? $_GET['s'] : 0;
 $js = !empty($_GET['no_js']) && $_GET['no_js'] == 1 ? 0 : 1;
 
 
 $tests = array(
   'raw' => array('raw'),
   'jquery' => array('jquery.min'),
+  'jquery-cache' => array('cache', 'jquery.min'),
+  'sizzle' => array('sizzle.min'),
+  'sizzle-cache' => array('cache', 'sizzle.min'),
   'core' => array('core.min'),
+  'core-cache' => array('cache', 'core.min'),
   'ajax' => array('core.min', 'ajax.min'),
+  'ajax-cache' => array('cache', 'core.min', 'ajax.min'),
   'attributes' => array('core.min', 'callbacks.min', 'support.min', 'attributes.min'),
+  'attributes-cache' => array('cache', 'core.min', 'callbacks.min', 'support.min', 'attributes.min'),
   'callbacks' => array('core.min', 'callbacks.min'),
+  'callbacks-cache' => array('cache', 'core.min', 'callbacks.min'),
   'css' => array('core.min', 'callbacks.min', 'support.min', 'css.min'),
+  'css-cache' => array('cache', 'core.min', 'callbacks.min', 'support.min', 'css.min'),
   'data' => array('core.min', 'data.min'),
+  'data-cache' => array('cache', 'core.min', 'data.min'),
   'deferred' => array('core.min', 'deferred.min'),
+  'deferred-cache' => array('cache', 'core.min', 'deferred.min'),
   'dimensions' => array('core.min', 'dimensions.min'),
-  'effects' => array('core.min', 'callbacks.min', 'support.min', 'effects.min'),
-  'event' => array('core.min', 'callbacks.min', 'support.min', 'attributes.min', 'data.min', 'event.min'),
+  'dimensions-cache' => array('cache', 'core.min', 'dimensions.min'),
+  'effects' => array('core.min', 'callbacks.min', 'effects.min'),
+  'effects-cache' => array('cache', 'core.min', 'callbacks.min', 'effects.min'),
+  'event' => array('core.min', 'callbacks.min', 'support.min', 'data.min', 'event.min'),
+  'event-cache' => array('cache', 'core.min', 'callbacks.min', 'support.min', 'data.min', 'event.min'),
   'manipulation' => array('core.min', 'callbacks.min', 'support.min', 'manipulation.min'),
+  'manipulation-cache' => array('cache', 'core.min', 'callbacks.min', 'support.min', 'manipulation.min'),
   'offset' => array('core.min', 'offset.min'),
+  'offset-cache' => array('cache', 'core.min', 'offset.min'),
   'queue' => array('core.min', 'queue.min'),
+  'queue-cache' => array('cache', 'core.min', 'queue.min'),
   'support' => array('core.min', 'callbacks.min', 'support.min'),
-  'traversing'  => array('core.min', 'traversing.min'),
+  'support-cache' => array('cache', 'core.min', 'callbacks.min', 'support.min'),
+  'traversing' => array('core.min', 'traversing.min'),
+  'traversing-cache' => array('cache', 'core.min', 'traversing.min'),
 );
 
 
@@ -50,24 +68,39 @@ $tests = array(
 <head>
   <meta charset="utf8">
   <title>jQuery perf rundown</title>
-  <?php if (!empty($query) && $component !== 'raw'): ?>
-  <script src="./scripts.php?<?php print implode("&", $query); ?>"></script>
-  <?php endif; ?>
+  <?php
+  if (!empty($query) && $component !== 'raw') {
+
+    $cache = !empty($_GET['cache']);
+
+    print '<script ' . (in_array('cache', (array) $component) ? 'type="text/cache"' : '') . '>';
+
+    if ($component !== 'raw') {
+      foreach ((array) $component as $c) {
+        if ($c !== 'cache') {
+          print "\n//$c\n" . load($c);
+        }
+      }
+    }
+
+    print '</script>';
+
+  }
+  ?>
 </head>
-<body style="text-align:center;">
+<body style="text-align:center;margin-top:20px;line-height:2;font-size:18px;">
 
 <?php
-  $links = array();
-  foreach ($tests as $id => $t) {
-    $links[] = '<a id="'. $id .'" href="?' . implode('&', qs($t)) . '">' . $id . '</a>';
-  }
-  print implode(' | ', $links);
-  ?>
-
+$links = array();
+foreach ($tests as $id => $t) {
+  $links[] = '<a id="' . $id . '" href="?' . implode('&', qs($t)) . '">' . $id . '</a>';
+}
+print implode(' | ', $links);
+?>
 <div id="log" style="text-align:center;font-size:100px;font:georgia;margin:50px"><h2>…</h2></div>
 <script><?php print str_replace(
-  array('"$component$"', '"$js$"', '"$samples$"'),
-  array(json_encode((array) $component), $js, $samples),
+  array('"$component$"', '"$samples$"'),
+  array(json_encode((array) $component), $samples),
   file_get_contents('./performance.min.js')
 ); ?></script>
 </body>
--- performance.js
+++ performance.js
@@ -10,7 +10,6 @@ if (window['performance']) {
         perf = window['performance'],
         timing = perf.timing,
         domLoading = timing.domLoading,
-        oldRaw = JSON.parse(localStorage.getItem('raw')) || [],
         oldC = JSON.parse(localStorage.getItem(c)) || [],
         get = [
           /*'domLoading',*/ 'domInteractive', 'domContentLoadedEventStart', 'domContentLoadedEventEnd', 'domComplete'
@@ -29,30 +28,29 @@ if (window['performance']) {
 
       function print (el, data) {
         var filC = oldC.filter(excludeMobile);
-        var filRaw = oldRaw.filter(excludeMobile);
         var excluded = oldC.length - filC.length;
         el.innerHTML = [
           '<h2 style="font-size:50px">', c.join(', ') , '</h2>',
           '<ul style="list-style:none;margin:0;padding:0;">',
             //'<li>', data.domContentLoadedEventEnd - data.domContentLoadedEventStart, '&thinsp;ms</li>',
             //'<li>', data.domComplete - data.domContentLoadedEventStart, '&thinsp;ms</li>',
-            '<li style="color:gray">', data.domComplete, '&thinsp;ms</li>',
+            '<li style="color:gray">', (data.domInteractive) + '&thinsp;ms ', '</li>',
             '<li>',
-              (filRaw.length && filC.length) ? Math.round(filC.reduce(sum) / filC.length - filRaw.reduce(sum) / filRaw.length) + '&thinsp;ms' : 'N/A',
+              Math.round(filC.reduce(sum) / filC.length) + '&thinsp;ms',
               (excluded > 0 ? '<br/><small style="font-size:12px!important;color:gray;">excluded: ' + excluded + '</small>' : ''),
             '</li>',
             //'<li><button type="button" onclick="window.location.reload();" style="font-weight:bold;font-size:20px;">RELOAD</button></li>',
           '</ul>',
-          '<p style="font-size:15px">' +
-            'Time in gray is the difference between domComplete and domLoading for this page<br/>' +
-            'Time in black is the difference between the raw time average and this page loading average.</p>',
+          '<p style="font-size:15px">',
+            'Time in gray is the difference between domComplete and domLoading for this page<br/>',
+            'Time in black is the domComplete average.</p>',
           '<button type="button" onclick="localStorage.clear();" style="color:red;">Clear data</button>'
         ].join('');
       }
 
       // transform dates to relative times
       for (n = 0, nl = get.length; n < nl; n += 1) {
-        data[get[n]] = timing[get[n]] - domLoading;
+        data[get[n]] = timing[get[n]] - timing.domLoading;
       }
 
       oldC.push(data.domComplete);
@@ -64,25 +62,12 @@ if (window['performance']) {
         print(log, data);
       }
       else {
-        // reload until we get 100 data points.
+        // reload until we get "minData" points.
         setTimeout(function () {
           var link = document.querySelector('a[href$="' + window.location.search.replace(/&r=.+$/, '') + '"]');
           window.location = link.getAttribute('href') + '&r='+ Math.random();
         }, 500);
       }
-      /*
-       var xhr = new XMLHttpRequest();
-       xhr.open("POST", 'log.php', true);
-       xhr.onreadystatechange = function (e) {
-       if (xhr.readyState === 4 && xhr.status === 200) {
-       var log = document.getElementById('log');
-       print(log, data);
-       }
-       };
-       xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-       xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-       xhr.send('data=' + json);
-       */
     }, 50);
   }, false);
 }
